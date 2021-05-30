632a1ec38bd998554cedf7be90f9d3ab9c420056
iampersistent <shank@zestic.com>
2021-05-29 23:10:03 -0700

    Update Button API

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export const Button: React.FC = () => {
-  return <button>Hello world</button>;
-};
+export const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<'button'>
+>(({ children, ...props }, ref) => {
+  return (
+      <button ref={ref} type="button" {...props}>
+        {children}
+      </button>
+  )
+});
 
-
+Button.displayName = 'Button';
